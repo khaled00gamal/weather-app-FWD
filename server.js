6cd1bd459c1218f9db34ac01bd9f676b85c52b04
Khaled gamal <60145969+khaled00gamal@users.noreply.github.com>
2021-11-19 18:53:25 +0200

    setting up the server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser=require("body-parser");
+const cors=require("cors");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("website"));
+
+let projectData = {};
+
+app.post("/sendData",function(req,res){
+projectData['date']=req.body.date;
+projectData['temp']=req.body.temp;
+projectData['content']=req.body.content;
+res.send(projectData);
+});
+
+app.get("/getData",function(req,res){
+res.send(projectData);
+});
+
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+  });
