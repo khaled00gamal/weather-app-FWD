cccec8d95e0ab1f8d082a651ca40230c882f3c6a
Khaled gamal <60145969+khaled00gamal@users.noreply.github.com>
2021-11-19 19:38:05 +0200

    added client get & post

--- website/app.js
+++ website/app.js
@@ -1,37 +1,57 @@
 /* Global Variables */
-const url="https://api.openweathermap.org/data/2.5/weather";
-const apiKey="8a7e82e36ec4c18d6f49916ce52b0131";
-
+const url = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "8a7e82e36ec4c18d6f49916ce52b0131";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-const generate = document.getElementById('generate');
+const generate = document.getElementById("generate");
 
-generate.addEventListener("click",function(e){
-    e.preventDefault();
-    const zip = document.getElementById("zip").value;
-    const content = document.getElementById("feelings").value;
+//chains all the promises together
+generate.addEventListener("click", function (e) {
+  e.preventDefault();
+  const zip = document.getElementById("zip").value;
+  const content = document.getElementById("feelings").value;
 
+  getData(url,zip,apiKey).then(function(data){
+      postData("/getData",{date: newDate,content:content,temp:data.main.temp});
+  }).then(function(){
+      //update the ui with the data recieved
+  })
+});
 
-})
-
-
-const getData = async function(url,zip,apiKey){
-    const res = fetch(`${url}?q=${zip}&appid=${apiKey}`);
-    try{
-        const jsonData= await res.json();
-
-    }catch(exception){
-        console.log("error",exception);
-    }
+//get data from api
+const getData = async function (url, zip, apiKey) {
+  const res = fetch(`${url}?q=${zip}&appid=${apiKey}`);
+  try {
+    const jsonData = await res.json();
+    return jsonData;
+  } catch (exception) {
+    console.log("error", exception);
+  }
 };
 
+//post new data
+const postData = async function (url = "", data = {}) {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      date: data.date,
+      temp: data.temp,
+      content: data.content,
+    })
+  });
 
+  try{
+      const newData = await res.json();
+      return newData;
 
-
-
-
-
-
+  } catch(exception){
+      console.log("error",exception);
+  }
+};
